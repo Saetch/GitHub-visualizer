e728973c5ce2df2bb9bd2d00d3663f8163b94f2a
nils.wriedt <nils.wriedt@tunnelsoft.com>
2026-06-15 13:11:34 +0200

    made a request to gharchive

--- archive_reader/src/main.rs
+++ archive_reader/src/main.rs
@@ -3,5 +3,10 @@
 async fn main() {
     let date = "2025-03-05";
     let hour = "12";
-    println!("Hello, world!");
+
+    let url = format!("https://data.gharchive.org/{date}-{hour}.json.gz");
+
+    let response = reqwest::get(&url).await.unwrap();
+
+    print!("{:?}", response);
 }
