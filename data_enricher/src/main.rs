d719ac7eaedb519347279f3234cb128810d29a8a
finn.labudda <finn.labudda@tunnelsoft.com>
2026-06-15 16:35:45 +0200

    subcribe to events

--- data_enricher/src/main.rs
+++ data_enricher/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::{Stream, StreamExt, TryStreamExt};
+use tokio::io;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_util::bytes::Bytes;
+use tokio_util::io::StreamReader;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let client = async_nats::connect("localhost:4222").await.unwrap();
+
+    let mut stream = client.subscribe("events").await.unwrap();
+    while let Some(msg) = stream.next().await {
+        println!("Received message: {:?}", msg);
+    }
 }
