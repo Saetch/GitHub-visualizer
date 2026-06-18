3a72c2d8eaa3ef900ae42e30642bfd5d7cfecfb6
finn.labudda <finn.labudda@tunnelsoft.com>
2026-06-18 13:28:31 +0200

    created the protocol

--- visualizer_protocol/src/lib.rs
+++ visualizer_protocol/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub enum GitEventMessage{
+    Placeholder{
+        location_x: f64,
+        location_y: f64,
+        event_description: String,
     }
 }
