790a867ca35525d979570041a5c695c11cde72d9
Saetch <nils.wriedt@hotmail.com>
2026-06-22 15:33:56 +0200

    consumer creation in ws_handler

--- websocket_server/src/main.rs
+++ websocket_server/src/main.rs
@@ -1,3 +1,5 @@
+use std::os::macos::raw::stat;
+use crate::jetstream::consumer::pull::Stream;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use actix_ws::{Message};
 use async_nats::jetstream;
@@ -9,8 +11,8 @@ use std::time::Duration;
 use tokio::sync::broadcast;
 
 struct AppState {
-    tx: broadcast::Sender<String>,
     counter: u64,
+    stream: jetstream::stream::Stream,
 }
 
 async fn ws_handler(
@@ -19,23 +21,31 @@ async fn ws_handler(
     state: web::Data<AppState>,
 ) -> actix_web::Result<HttpResponse> {
     let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body)?;
-    let mut rx = state.tx.subscribe();
-
+    let consumer = state.stream.get_or_create_consumer(
+        "ws-gateway",
+        PullConsumerConfig {
+            durable_name: Some("ws-gateway".to_string()),
+            filter_subject: "events_ordered".to_string(),
+            ack_policy: AckPolicy::Explicit,
+            ack_wait: Duration::from_secs(30),
+            deliver_policy: DeliverPolicy::All,
+            ..Default::default()
+        },
+    ).await.expect("consumer setup failed");
+    let mut messages = consumer.messages().await.expect("consumer stream failed");
     actix_web::rt::spawn(async move {
         loop {
             tokio::select! {
                 // NATS event -> WebSocket client
-                event = rx.recv() => {
+                Some(event) = messages.next() => {
                     match event {
                         Ok(text) => {
-                            if session.text(text).await.is_err() {
-                                break;
-                            }
+                            println!("{}", String::from_utf8(text.payload.to_vec()).unwrap());
                         }
-                        Err(broadcast::error::RecvError::Lagged(n)) => {
-                            eprintln!("Client lagged, dropped {} messages", n);
-                        }
-                        Err(_) => break,
+                        Err(e) => {
+                            eprintln!("NATS error: {}", e);
+                            break;
+                        },
                     }
                 }
 
@@ -74,38 +84,18 @@ async fn main() -> std::io::Result<()> {
         ..Default::default()
     }).await.expect("stream setup failed");
 
-    let consumer = stream.get_or_create_consumer(
-        "ws-gateway",
-        PullConsumerConfig {
-            durable_name: Some("ws-gateway".to_string()),
-            filter_subject: "events_ordered".to_string(),
-            ack_policy: AckPolicy::Explicit,
-            ack_wait: Duration::from_secs(30),
-            deliver_policy: DeliverPolicy::All,
-            ..Default::default()
-        },
-    ).await.expect("consumer setup failed");
+
 
     let (tx, _rx) = broadcast::channel::<String>(1024);
     let tx = Arc::new(tx);
 
-    // NATS pump task
     {
         let tx = tx.clone();
-        tokio::spawn(async move {
-            let mut messages = consumer.messages().await.expect("consumer stream failed");
-            while let Some(Ok(msg)) = messages.next().await {
-                if let Ok(text) = String::from_utf8(msg.payload.to_vec()) {
-                    let _ = tx.send(text);
-                }
-                let _ = msg.ack().await;
-            }
-        });
     }
 
     let state = web::Data::new(AppState {
-        tx: (*tx).clone(),
         counter: 0,
+        stream,
     });
 
     HttpServer::new(move || {
