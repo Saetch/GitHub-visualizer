8873cb8291f546e5e99d63622434692ba67c5f8f
Saetch <nils.wriedt@hotmail.com>
2026-07-06 13:48:23 +0200

    updated archive reader to continuously query the newest hour's data

--- archive_reader/src/main.rs
+++ archive_reader/src/main.rs
@@ -7,53 +7,101 @@ use async_nats::jetstream::stream::{
     StorageType,
 };
 use futures_util::TryStreamExt;
-use std::time::Duration;
+use std::time::Duration as StdDuration;
 use tokio::io::{self, AsyncBufReadExt, BufReader};
 use tokio_util::io::StreamReader;
+use chrono::{Timelike, Utc};
+use tokio::time::sleep;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let now = std::time::SystemTime::now();
-    let date = "2025-03-05";
-    let hour = "17";
-
-    let url = format!("https://data.gharchive.org/{date}-{hour}.json.gz");
-
-    let response = reqwest::get(&url).await.unwrap();
-    let stream = response.bytes_stream();
-    let stream = stream.map_err(|err| {
-        io::Error::new(io::ErrorKind::Other, err)
-    });
-    let reader = StreamReader::new(stream);
-    let decoder = GzipDecoder::new(reader);
-    let mut lines = BufReader::new(decoder).lines();
-    let client = async_nats::connect("nats:4222").await.unwrap();
-    let jetstream = async_nats::jetstream::new(client);
-    jetstream
-        .get_or_create_stream(StreamConfig {
-            name: "GHARCHIVE".to_string(),
-            subjects: vec!["events".to_string()],
-            storage: StorageType::File,
-            retention: RetentionPolicy::Limits,
-            discard: DiscardPolicy::Old,
-            max_age: Duration::from_secs(24 * 60 * 60),
-            max_bytes: 5 * 1024 * 1024 * 1024,
-            ..Default::default()
-        })
-        .await
-        .unwrap();
-    let mut line_nmbr = 0;
-    while let Some(line) = lines.next_line().await.unwrap() {
-        let ack = jetstream.publish("events", line.into()).await.unwrap();
-        line_nmbr += 1;
-        if line_nmbr % 1000 == 0 {
-            println!("Processed {} lines", line_nmbr);
+    let mut previous_stamp:String = "".to_string();
+    let mut completed = false;
+    loop {
+        let current_stamp = last_completed_gharchive_hour();
+        if current_stamp==previous_stamp{
+            sleep(StdDuration::from_mins(5)).await;
+        }else { completed = false; }
+        if completed{
+            break;
         }
-    }
-    println!("Done");
+        previous_stamp = current_stamp;
+        let now = std::time::SystemTime::now();
+        let url = format!("https://data.gharchive.org/{previous_stamp}.json.gz");
+        println!("Local clock shows: {:?}", &now);
+        print!("Downloading {} ...", &url);
+        let response = reqwest::get(&url).await;
+        if response.is_err() {
+            println!("Failed to download {}, waiting 5 minutes", &url);
+            continue;
+        }else {
+            println!("Connection established to {} successfully", &url);
+        }
+        let response = response.unwrap();
+        println!("Response code: {}", response.status());
+        if response.status().is_success(){
+            println!("Downloaded {} successfully", &url);
+        }else {
+            println!("Failed to download {}, waiting 5 minutes", &url);
+            continue;
+        }
+        let stream = response.bytes_stream();
+        let stream = stream.map_err(|err| {
+            io::Error::new(io::ErrorKind::Other, err)
+        });
+        let reader = StreamReader::new(stream);
+        let decoder = GzipDecoder::new(reader);
+        let mut lines = BufReader::new(decoder).lines();
+        let client = async_nats::connect("localhost:4222").await.unwrap();
+        let jetstream = async_nats::jetstream::new(client);
+        jetstream
+            .get_or_create_stream(StreamConfig {
+                name: "GHARCHIVE".to_string(),
+                subjects: vec!["events".to_string()],
+                storage: StorageType::File,
+                retention: RetentionPolicy::Limits,
+                discard: DiscardPolicy::Old,
+                max_age: StdDuration::from_secs(24 * 60 * 60),
+                max_bytes: 5 * 1024 * 1024 * 1024,
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+        let mut line_nmbr = 0;
+        while let Some(line) = lines.next_line().await.unwrap() {
+            let ack = jetstream.publish("events", line.into()).await.unwrap();
+            line_nmbr += 1;
+            if line_nmbr % 1000 == 0 {
+                println!("Processed {} lines", line_nmbr);
+            }
+        }
+        println!("Done");
 
-    let end = std::time::SystemTime::now();
-    let elapsed = end.duration_since(now).unwrap();
-    println!("Elapsed: {:?} ms", elapsed.as_millis());
-    println!("Processed {} lines", line_nmbr);
+        let end = std::time::SystemTime::now();
+        let elapsed = end.duration_since(now).unwrap();
+        println!("Elapsed: {:?} ms", elapsed.as_millis());
+        println!("Processed {} lines", line_nmbr);
+        completed = true;
+        sleep(StdDuration::from_mins(5)).await
+    }
+
+
+}
+
+
+
+fn last_completed_gharchive_hour() -> String {
+    let now = Utc::now();
+
+    let last_hour = now
+        .with_minute(0).unwrap()
+        .with_second(0).unwrap()
+        .with_nanosecond(0).unwrap()
+        - StdDuration::from_hours(1);
+
+    format!(
+        "{}-{}",
+        last_hour.format("%Y-%m-%d"),
+        last_hour.hour()
+    )
 }
